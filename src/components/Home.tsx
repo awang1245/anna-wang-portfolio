40dac936ff833e1c25c54b613bb47fea23f52d1c
Anna Wang <anna_wang@brown.edu>
2024-02-20 04:36:36 -0500

    feat: sushi conveyor belt working

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,5 @@
 import "../styles/Home.css";
+import "@dotlottie/player-component";
 
 function Home() {
   return (
@@ -12,12 +13,46 @@ function Home() {
               <br />
               Computer Science and Business at Brown University.
             </div>
-            <p className="intro-details">
+            <p>
               I'm interested in the intersection of technology and design, and
               my goal is to develop <br />
               interfaces that foster meaningful relationships between users and
               products.
             </p>
+          </div>
+          <script
+            src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs"
+            type="module"
+          ></script>
+          <dotlottie-player
+            src="https://lottie.host/58ba3c4d-3096-45b3-a6e8-9d34dacf933d/6uHUa42zzw.json"
+            background="transparent"
+            speed="1"
+            loop
+            autoplay
+          ></dotlottie-player>
+          <div className="marquee">
+            <div className="belt-items">
+              <img src="src/assets/ikura.svg" />
+              <img src="src/assets/shrimp.svg" />
+              <img src="src/assets/tamago.svg" />
+              <img src="src/assets/maki.svg" />
+              <img src="src/assets/salmon.svg" />
+              <img src="src/assets/ducky.svg" />
+              <img src="src/assets/tako.svg" />
+              <img src="src/assets/tuna.svg" />
+              <img src="src/assets/ikura.svg" />
+              <img src="src/assets/shrimp.svg" />
+              <img src="src/assets/tamago.svg" />
+              <img src="src/assets/maki.svg" />
+              <img src="src/assets/salmon.svg" />
+              <img src="src/assets/ducky.svg" />
+              <img src="src/assets/tako.svg" />
+              <img src="src/assets/tuna.svg" />
+            </div>
+          </div>
+          <div className="welcome">
+            Welcome, grab a snack, and enjoy your stay.
           </div>
         </div>
       </main>
