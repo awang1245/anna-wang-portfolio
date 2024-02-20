c6fec59147d8423b835c5ddd618c470464e6fbf6
Anna Wang <anna_wang@brown.edu>
2024-02-20 01:17:48 -0500

    feat: custom cursor + starting landing page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,12 +5,20 @@ function Home() {
     <>
       <main>
         <div className="home">
-          <img src="src/assets/logo.svg" className="delete" />
-          <img src="src/assets/logo.svg" className="delete" />
-          <img src="src/assets/logo.svg" className="delete" />
-          <img src="src/assets/logo.svg" className="delete" />
-          <img src="src/assets/logo.svg" className="delete" />
-          <img src="src/assets/logo.svg" className="delete" />
+          <div className="text">
+            <div className="intro">
+              Hi, I'm Anna. <br />
+              I'm a UI developer and designer studying
+              <br />
+              Computer Science and Business at Brown University.
+            </div>
+            <p className="intro-details">
+              I'm interested in the intersection of technology and design, and
+              my goal is to develop <br />
+              interfaces that foster meaningful relationships between users and
+              products.
+            </p>
+          </div>
         </div>
       </main>
     </>
