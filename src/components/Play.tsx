3ec9eb6001f1848128b9757a7bd462e2667bd5e4
Anna Wang <anna_wang@brown.edu>
2024-05-06 16:06:01 -0400

    feat: added placeholder text to play page

--- src/components/Play.tsx
+++ src/components/Play.tsx
@@ -1,7 +1,17 @@
+import "../styles/Play.css";
+import { useRecoilState } from "recoil";
+import { isDarkState } from "./atoms";
+
 function Play() {
+  const [isDark] = useRecoilState<boolean>(isDarkState);
+
   return (
     <>
-      <main></main>
+      <main className={isDark ? "page-dark" : "page-light"}>
+        <div className="play">
+          <p>Nothing to see here yet! Check back in Summer 2024.</p>
+        </div>
+      </main>
     </>
   );
 }
