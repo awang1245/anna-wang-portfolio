9209c8b22b5dd6771b2270160459a762d00da913
Anna Wang <anna_wang@brown.edu>
2024-12-20 16:13:10 -0500

    fix: import error

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app.tsx";
 import "./styles/index.css";
 import { HashRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
+import App from "./app";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
