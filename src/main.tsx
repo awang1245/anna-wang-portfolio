b10dbb6c340526eb2aae1edad75a313de46f0c1c
Anna Wang <anna_wang@brown.edu>
2024-12-20 16:32:42 -0500

    rahh idk anymore

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
+import "./styles/index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/index.css";
 import { HashRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import App from "./app";
